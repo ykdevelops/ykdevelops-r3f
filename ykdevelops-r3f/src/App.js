32f4343dc57b935ea6ba975cd34c787f85eadb3b
ykdevelops <ykhalil@quotemedia.com>
2023-02-28 23:58:14 -0500

    feat: :zap: Installing and deploying to firebase

--- ykdevelops-r3f/src/App.js
+++ ykdevelops-r3f/src/App.js
@@ -44,7 +44,7 @@ function App() {
       id="canvas-container"
       style={{ top: 0, height: '100vh', position: 'relative' }}
     >
-      <Canvas camera={{ fov: 5.1, position: [10, -30, 0], maxPolarAngle: Math.PI / 3 }}>
+      <Canvas camera={{ fov: 5.1, position: [30, -30, 0], maxPolarAngle: Math.PI / 3 }}>
         <OrbitControls
           enablePan={false}
           enableRotate={true}
@@ -54,10 +54,10 @@ function App() {
           minAzimuthAngle={0.1}
           maxAzimuthAngle={Math.PI / 4}
         />
-        <mesh position={[0, -1.3, 0.25]} rotation={[0, 200, 0]}>
+        <mesh position={[0, -1.7, 0.25]} rotation={[0, 200, 0]}>
           <primitive object={gltf.scene} />
         </mesh>
-        <pointLight color={0xffffe6} intensity={1} position={[10, 10, 0]} />
+        <pointLight color={0xffffe6} intensity={1} position={[10, 10, 4]} />
         <pointLight color={0xffffe6} intensity={1} position={[-10, 10, 0]} />
       </Canvas>
       {clicked && <Popup closePopup={closePopup} />}
