544cf77fa38b8bc905249417f585037748907664
ykdevelops <ykdevelops@gmail.com>
2023-03-27 12:43:49 -0400

    feat: :fire: Updated Resume files

--- ykdevelops-r3f/src/components/Canvas/Resume.js
+++ ykdevelops-r3f/src/components/Canvas/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from "../../assets/videos/resume2.png";
+import img from "../../assets/videos/resume.jpg";
 import { useLoader } from '@react-three/fiber';
 import * as THREE from 'three';
 
