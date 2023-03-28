1741edc322167d0f92fc1c47b83c173f99da079c
ykdevelops <ykdevelops@gmail.com>
2023-03-28 17:09:11 -0400

    style: :art: Added styling to Resume

--- ykdevelops-r3f/src/components/Resume.js
+++ ykdevelops-r3f/src/components/Resume.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import currentFile from '../assets/videos/resume.pdf';
+import '../styles/resume.css'
 export default function Resume() {
     return (
-        <div>
+        <div className='resume-page'>
             <iframe
                 src={currentFile}
                 title="Resume PDF"
-                style={{ paddingTop: '100px', width: '100vw', height: '100vh', border: 'none', backgroundColor: '#323639' }}
+                style={{ paddingTop: '50px', width: '100vw', height: '100vh', border: 'none', backgroundColor: '#323639' }}
             />
         </div>
     )
