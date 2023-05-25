b47104112bb632be1feeab9f6bdfb0f63a5b97c5
ykdevelops <ykdevelops@gmail.com>
2023-05-24 23:30:01 -0400

    feat: :zap: Removed routing and made repo exclusively for r3f studio

--- ykdevelops-r3f/src/App.js
+++ ykdevelops-r3f/src/App.js
@@ -1,29 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from './components/Navbar';
+
 import CanvasR3F from './components/Canvas/CanvasR3F';
-import About from './components/About';
-import Resume from './components/Resume.js';
-import Projects from './components/Projects';
-import ComingSoon from './components/ComingSoon';
-import NotFound from './components/NotFound';
-import LoaderMain from './components/LoaderMain';
 
 function App() {
   return (
     <React.StrictMode>
       <div>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/studio" element={<CanvasR3F />} />
-            <Route path="/comingSoon" element={<ComingSoon />} />
-            <Route element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <CanvasR3F />
       </div>
     </React.StrictMode>
+
   );
 }
 
--- ykdevelops-r3f/src/components/About.js
+++ ykdevelops-r3f/src/components/About.js
@@ -1,41 +1,39 @@
-import React from 'react';
-import '../styles/about.css';
+import React, { Suspense } from 'react';
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
 import { FaYoutubeSquare } from "react-icons/fa";
 import { AiFillFilePdf } from "react-icons/ai";
 import currentFile from '../assets/videos/resume.pdf';
-import { Suspense } from 'react';
+import styles from '../styles/About.module.css';
+// Other imports...
 
 const About = () => {
     return (
-        <div className='about-page'>
+        <div className={styles.aboutPage}>
 
-            <div className='headshot-row'>
-                <Suspense fallback={<div className='loading-image'></div>}>
-                    <img className='headshot' src="/aboutImage.png" alt="headshot" />
+            <div className={styles.headshotRow}>
+                <Suspense fallback={<div className={styles.loadingImage}></div>}>
+                    <img className={styles.headshot} src="/aboutImage.png" alt="headshot" />
                 </Suspense>
-                <h2 className='welcome-statement'>
+                <h2 className={styles.welcomeStatement}>
                     <div>Hello,</div>
                     <div>I'm Youssef!</div>
                 </h2>
             </div>
 
-            <div className='yellow-box-row'>
-                <div className='yellow-box'>
-                    <div className='icon-column' >
-                        <abbr title="My Resume"><a href={currentFile} className='icon-row' target="_blank" rel="noreferrer"><AiFillFilePdf className="about-social-icon" /> </a></abbr>
-                        <abbr title="My LinkedIn"> <a href="https://linkedin.com/in/ykdevelops" className='icon-row' target="_blank" rel="noreferrer"><FaLinkedin className="about-social-icon" /> </a></abbr>
-                        <abbr title="My Github"><a href="https://github.com/ykdevelops" className='icon-row' target="_blank" rel="noreferrer"><FaGithubSquare className="about-social-icon" /></a></abbr>
-                        <abbr title="My Youtube"><a href="https://www.youtube.com/channel/UCRIft9RM1NOq6m0MIJeiJJg" className='icon-row' target="_blank" rel="noreferrer"><FaYoutubeSquare className="about-social-icon" /></a></abbr>
-
-
+            <div className={styles.yellowBoxRow}>
+                <div className={styles.yellowBox}>
+                    <div className={styles.iconColumn} >
+                        <abbr title="My Resume"><a href={currentFile} className={styles.iconRow} target="_blank" rel="noreferrer"><AiFillFilePdf className={styles.aboutSocialIcon} /> </a></abbr>
+                        <abbr title="My LinkedIn"> <a href="https://linkedin.com/in/ykdevelops" className={styles.iconRow} target="_blank" rel="noreferrer"><FaLinkedin className={styles.aboutSocialIcon} /> </a></abbr>
+                        <abbr title="My Github"><a href="https://github.com/ykdevelops" className={styles.iconRow} target="_blank" rel="noreferrer"><FaGithubSquare className={styles.aboutSocialIcon} /></a></abbr>
+                        <abbr title="My Youtube"><a href="https://www.youtube.com/channel/UCRIft9RM1NOq6m0MIJeiJJg" className={styles.iconRow} target="_blank" rel="noreferrer"><FaYoutubeSquare className={styles.aboutSocialIcon} /></a></abbr>
                     </div>
-                    <div className='description-column'>
-                        <p className='description-text'>
+                    <div className={styles.descriptionColumn}>
+                        <p className={styles.descriptionText}>
                             As a developer with over a year of experience, I specialize in creating responsive web applications using Javascript frameworks like React and Vue. I am passionate about staying up-to-date with the latest online technologies and using them to create innovative and engaging user experiences.
                         </p>
-                        <p className='description-text'>
+                        <p className={styles.descriptionText}>
                             Recently, I have also been diving into Next.js, and I am enthusiastic about the potential it offers for building performant, scalable, and feature-rich web applications.
                         </p>
                     </div>
